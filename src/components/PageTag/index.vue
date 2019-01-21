a2fea27657c51eb6ab8e4f227f9fc9c7f5124923
edwardsbean <edwardsbean@gmail.com>
2019-01-21 23:27:22 +0800

    add page tag

---
+++ src/components/PageTag/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="display: flex;justify-content: flex-end">
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="items.page"
+      :page-sizes="[10,20,50, 100, 200, 400, 500]"
+      :page-size="items.pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="items.total">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PageTag',
+    props: ['items'],
+    methods: {
+      handleSizeChange(val) {
+        var page = 1
+        var size = val
+        this.$emit('reload', { size, page })
+      },
+      handleCurrentChange(val) {
+        var size = this.items.pageSize
+        var page = val
+        this.$emit('reload', { size, page })
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'normalize.css/normalize.css'// A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import locale from 'element-ui/lib/locale/lang/zh-CN' // 饿了么的语言包
 
 import '@/styles/index.scss' // global css
 
@@ -15,10 +15,20 @@ import store from './store'
 import '@/icons' // icon
 import '@/permission' // permission control
 
+import * as filters from './filters' // global filters
+
+import PageTag from '@/components/PageTag/index.vue'
+
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
 if (process.env.NODE_ENV === 'mock') {
   require('./mock') // simulation data
 }
 Vue.use(ElementUI, { locale })
+Vue.component('page-tag', PageTag)
 
 Vue.config.productionTip = false
 
